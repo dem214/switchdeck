fda0dfe2c239516270b313f378d894fffafa6574
Dzmitry Izaitka <dem214overlord@gmail.com>
2019-09-22 15:33:13 +0300

    django_heroku settings

--- switchdecksite/settings.py
+++ switchdecksite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOCALE_PATHS = ['locale']
 
 COMMENTS_PER_PAGE = 10
+
+#Activate django-heroku
+django_heroku.settings(locals())
