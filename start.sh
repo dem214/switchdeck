cbe79f0e86a2ca17fb28c33b2b5863f6d8015a95
Dzmitry Izaitka <dem214overlord@gmail.com>
2021-03-06 20:43:02 +0300

    nginx ready

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 python /app/manage.py migrate
 # /bin/python3 /app/manage.py compilemessages
 python /app/manage.py collectstatic -c
-gunicorn -w 4 switchdeck.wsgi --bind="0.0.0.0:8000"
+gunicorn -w 4 switchdeck.wsgi --bind="0.0.0.0:8000" --forwarded-allow-ips="nginx"
