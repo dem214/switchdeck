93bba0807f801405765c7d188ed2150c58761260
Dzmitry Izaitka <dem214overlord@gmail.com>
2021-03-07 19:05:41 +0300

    catalog_service localization

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python /app/manage.py migrate
-# /bin/python3 /app/manage.py compilemessages
+python /app/manage.py compilemessages
 python /app/manage.py collectstatic -c
 gunicorn switchdeck.wsgi
